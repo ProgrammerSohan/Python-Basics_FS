af2b0872dfaa6adab3025ba5d1e108881543d34f
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-25 22:14:48 +0600

    strings_a

---
+++ strings.py
@@ -0,0 +1,24 @@
+#print("Hello Sohan")
+#print("I don't want to go")
+#myvar = "My name is Sohan"
+#print(myvar)
+#print( myvar.upper() )
+
+#myvar2 = "hello sohan how are you?"
+#print( myvar2.capitalize() )
+#print( myvar2.split() )
+
+#myvar3 ="Hi there, this is sohan"
+#print( myvar3.split(",") )
+
+myvar4 = "How are you John?"
+#print( myvar4.split("o") )
+#print( myvar4[0] )
+#print ( myvar4[6])
+#print ( myvar4[-1] )
+print (myvar4[0:5])
+
+
+
+
+
